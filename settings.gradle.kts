2dd57a4336413fceb3a1c019df66d2ff8da1209e
Anand Vaibhav <knpaswan1974@gmail.com>
2025-09-25 21:21:32 +0530

    [Phase 1] Add: Base project scaffold

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+pluginManagement{repositories{google();mavenCentral();gradlePluginPortal()}} dependencyResolutionManagement{repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS);repositories{google();mavenCentral()}} rootProject.name="Xhyne"
+include(":app")
