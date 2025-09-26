dbf8376b5a4aeaf969d93f72d5da646ffae1318b
Anand Vaibhav <knpaswan1974@gmail.com>
2025-09-26 06:05:23 +0530

    [Phase 1] AI-Gen: Base project scaffold

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
-pluginManagement{repositories{google();mavenCentral();gradlePluginPortal()}} dependencyResolutionManagement{repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS);repositories{google();mavenCentral()}} rootProject.name="Xhyne"
+rootProject.name = "Xhyne"
 include(":app")
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
